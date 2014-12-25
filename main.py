7b5b850e5d40d5b4700dc862f1203b188c5a2ff4
Timothée Lacroix <timothee.lax@gmail.com>
2014-12-25 11:23:03 +0100

    updated tex. clearer main

--- main.py
+++ main.py
@@ -21,20 +21,28 @@ def applyLearner(learner, graph, losses, horizon=100, repeat=1):
 
     return np.mean(np.cumsum(regrets, axis=1), axis=0)
 
+n_arms=6
+eps=0.1
+
+losses = Losses([Bernoulli(x) for x in [0.5] + [0.5-eps]*(n_arms-1)])
+
+graph = ERGraph(arms=n_arms, r=0.5)
 
 
-learner = EXP3(gamma=0.01, eta=0.01, arms=3)
-graph = ERGraph(arms=3, r=0.5)
-losses = Losses([Bernoulli(0.2), Bernoulli(0.3), Bernoulli(0.4)])
+learner = EXP3(gamma=0.01, eta=0.01, arms=n_arms)
+regrets = applyLearner(
+    learner, graph, losses,
+    horizon=1000, repeat=100
+)
 
-regrets = applyLearner(learner, graph, losses, horizon=1000, repeat=100)
-pylab.plot(regrets,'r-')
-pylab.show()
+pylab.plot(regrets,'r-',label='EXP3')
 
-learner = DuplEXP3(arms=5)
-graph = ERGraph(arms=5, r=0.5)
-losses = Losses([Bernoulli(0.2), Bernoulli(0.3), Bernoulli(0.4), Bernoulli(0.6), Bernoulli(0.8)])
-# il faut r<logT/2N, donc là j'ai mis un T tout petit...
-regrets = applyLearner(learner, graph, losses, horizon=10, repeat=10)
-pylab.plot(regrets,'m-')
+learner = DuplEXP3(arms=n_arms)
+regrets = applyLearner(
+    learner, graph, losses,
+    horizon=1000, repeat=100
+)
+
+pylab.plot(regrets,'b-',label='DuplEXP3')
+
 pylab.show()
