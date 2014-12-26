3136d9ea02eaebf5a5c5028c9102f9a6bf80dceb
judithabk6 <judithabk6@gmail.com>
2014-12-26 08:42:45 +0100

    figure and some new importations...

--- learners.py
+++ learners.py
@@ -1,5 +1,5 @@
 import numpy as np
-from numpy.random import multinomial
+from numpy.random import multinomial, randint
 
 
 class BaseLearner(object):
@@ -79,5 +79,31 @@ class DuplEXP3(BaseLearner):
         return lhat
 
 
+class EstimatingR(BaseLearner):
+    def __init__(self, k, C, graph, **kwargs):
+        super(EstimatingR, self).__init__(**kwargs)
+        self.k = k
+        self.C = C
+        self.graph = graph
 
-
+    def getEstimate(self):
+        j = 0
+        c = 0
+        M = np.zeros(self.k)
+        for t in range(self.C):
+            I = randint(self.arms)
+            Obs = self.graph.getObserved(I)
+            c += np.sum(Obs[list(range(self.arms).pop(I))])
+        if c/(self.C*(self.arms - 1)) <= 3/(2*self.arms):
+            return 0
+        else:
+            for t in range(self.C, T):
+                I = randint(self.arms)
+                Obs = self.graph.getObserved(I)
+                for i in range(self.arms):
+                    M[j] += int(i != I)
+                    j += Obs(i) * int(i != I)
+                    if j == k:
+                        return (np.max(M) + 1) ** (-1)
+                    else:
+                        M[j] = 0
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-import pylab
+import matplotlib.pyplot as plt
 
 from losses import *
 from arms import *
@@ -20,24 +20,37 @@ def applyLearner(learner, graph, losses, horizon=100, repeat=1):
 
     return np.mean(np.cumsum(regrets, axis=1), axis=0)
 
-n_arms=20
-eps=0.1
+n_arms = 20
+eps = 0.1
+n_iterations = 5000
+r = 0.5
 
 losses = Losses([Bernoulli(x) for x in [0.5] + [0.5+eps]*(n_arms-1)])
 
-graph = ERGraph(arms=n_arms, r=0.5)
+graph = ERGraph(arms=n_arms, r=r)
 
 
 learner = EXP3(gamma=0.01, eta=0.01, arms=n_arms)
 regrets = applyLearner(
     learner, graph, losses,
-    horizon=5000, repeat=100
+    horizon=n_iterations, repeat=100
 )
-pylab.plot(regrets,'r-',label='EXP3')
+plt.figure(1)
+plt.plot(regrets, 'r-', label='EXP3', linewidth=2)
 learner = DuplEXP3(arms=n_arms)
 regrets = applyLearner(
     learner, graph, losses,
-    horizon=5000, repeat=100
+    horizon=n_iterations, repeat=100
 )
-pylab.plot(regrets,'b-',label='DuplEXP3')
-pylab.show()
+plt.plot(regrets, 'b-', label='DuplEXP3', linewidth=2)
+upper_bound = 4 * np.sqrt((n_iterations / r + n_arms**2) * np.log(n_arms)) + \
+    np.sqrt(n_iterations)
+plt.plot(upper_bound * np.ones(n_iterations), label='Upper Bound', linewidth=2,
+         color='purple', linestyle="-")
+plt.legend(loc=2, fontsize=20)
+plt.xlabel('iterations', fontsize=20)
+plt.ylabel('cumulated regret', fontsize=20)
+
+plt.savefig('dupl_big_r.pdf')
+
+
