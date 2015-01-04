f18f37d943c85d075181c211309a7a2352df4e1f
Timothée Lacroix <timothee.lax@gmail.com>
2015-01-04 12:43:37 +0100

    added ER to test_borne_r

--- test_borne_r.py
+++ test_borne_r.py
@@ -4,24 +4,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-sizes = [25, 50, 75]
-ms = [1, 2, 5, 10]
+# sizes = [25, 50, 75]
+# ms = [1, 2, 5, 10]
 
-repeats = 150
+# repeats = 150
+
+# for n_nodes in sizes:
+#     pp = PdfPages('independence_number{}.pdf'.format(n_nodes))
+#     print(n_nodes)
+#     for m in ms:
+#         tirages = list()
+#         for i in range(repeats):
+#             print(i)
+#             g = Graph.Barabasi(n_nodes, m)
+#             r = 2*g.ecount()/(float(n_nodes)*(n_nodes-1))
+#             tirages.append(g.independence_number())
+#         plt.figure()
+#         plt.hist(tirages, normed=1)
+#         plt.title('{} nodes and m = {}, r = {}, 1/r = {}'.format(n_nodes, m, np.round(r, decimals=1), np.round(1/r, decimals=2)), fontsize=25)
+#         pp.savefig()
+#         plt.close()
+
+#     pp.close()
+
+sizes = [25, 50, 75]
+rs = [0.05, 0.2, 0.5, 0.7]
+
+repeats = 300
 
 for n_nodes in sizes:
-    pp = PdfPages('independence_number{}.pdf'.format(n_nodes))
+    pp = PdfPages('independence_number_ER_{}.pdf'.format(n_nodes))
     print(n_nodes)
-    for m in ms:
+    for r in rs:
         tirages = list()
         for i in range(repeats):
             print(i)
-            g = Graph.Barabasi(n_nodes, m)
-            r = 2*g.ecount()/(float(n_nodes)*(n_nodes-1))
+            g = Graph.Erdos_Renyi(n_nodes, r)
             tirages.append(g.independence_number())
         plt.figure()
         plt.hist(tirages, normed=1)
-        plt.title('{} nodes and m = {}, r = {}, 1/r = {}'.format(n_nodes, m, np.round(r, decimals=1), np.round(1/r, decimals=2)), fontsize=25)
+        plt.title('{} nodes and r = {}, r = {}, 1/r = {}'.format(n_nodes, r, np.round(r, decimals=1), np.round(1/r, decimals=2)), fontsize=25)
         pp.savefig()
         plt.close()
 
